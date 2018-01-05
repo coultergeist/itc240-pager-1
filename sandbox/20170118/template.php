58b9bea26c619dd8ddfb4183a386cfa7d6dd2082
DM Coulter <dcoult01@seattlecentral.edu>
2018-01-05 13:29:58 -0800

    Add files via upload

---
+++ sandbox/20170118/template.php
@@ -0,0 +1,13 @@
+<!--  -->
+<!-- Template for header and footer cutups    
+
+-->
+
+<?php include 'includes/header.php'?>
+
+<p>Awesome content here</p>
+
+<?php include 'includes/footer.php'?>
+
+       
+
