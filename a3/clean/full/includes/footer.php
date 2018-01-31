97d446bb38785c418a0a224c566a3c3856292620
DM Coulter <dcoult01@seattlecentral.edu>
2018-01-31 09:59:41 -0800

    Add footer.php
    Add footer to full template includes folder

---
+++ a3/clean/full/includes/footer.php
@@ -0,0 +1,34 @@
+<!-- footer begins here -->
+<!-- Dynamic Date: I tried to use &copy; php format: $now=new DateTime(); echo $now->format('Y'); ?>
+        and Copyright &copy; <=date("Y")?> but couldn't get either to work 
+-->
+
+</div>
+            </div>
+            <!-- /.row -->
+        </div>
+        <!-- /.container -->
+    </section>
+
+    <!-- Footer -->
+    <footer>
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <p class="copyright text-muted">Copyright &copy; <?=date("Y")?>
+                </div>
+            </div>
+            <!-- /.row -->
+        </div>
+        <!-- /.container -->
+    </footer>
+
+    <!-- jQuery -->
+    <script src="js/jquery.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="js/bootstrap.min.js"></script>
+
+</body>
+
+</html>
